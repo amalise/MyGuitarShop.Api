3fb94aa8f6e35a914276514c3ce339ae8ef3fe5e
Victoria Mariger <victoria.mariger@gmail.com>
2025-10-31 14:59:04 -0700

    Completed SQLConnection

---
+++ MyGuitarShop.Data.Ado/Factories/SqlConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace MyGuitarShop.Data.Ado.Factories;
+
+public class SqlConnectionFactory(string connectionString)
+{
+	public SqlConnection OpenSqlConnection()
+	{
+		var connection = new SqlConnection(connectionString);
+		connection.Open();
+		return connection;
+	}
+
+}
