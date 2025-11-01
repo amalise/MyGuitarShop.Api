2365e2c27c25a80d6a18a759786b342101725bc1
Victoria Mariger <victoria.mariger@gmail.com>
2025-11-01 13:01:45 -0700

    Completed HealthController.

---
+++ MyGuitarShop.Api/Controllers/HealthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyGuitarShop.Data.Ado.Factories;
+
+namespace MyGuitarShop.Api.Controllers
+{
+	public class HealthController(
+		ILogger<HealthController> logger,
+		SqlConnectionFactory sqlConnectionFactory)
+		: Controller
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			try
+			{
+				return Ok("Healthy");
+			}
+			catch (Exception)
+			{
+				logger.LogWarning("Health check failed unreasonably.");
+
+				return StatusCode(503, "Unhealthy");
+			}
+		}
+
+		[HttpGetAttribute("db")]
+		public IActionResult GetDbHealth()
+		{
+			try
+			{
+				using var connection = sqlConnectionFactory.OpenSqlConnection();
+
+				return Ok(new { Message = "Connection successful!", connection.Database });
+
+			}
+			catch (Exception)
+			{
+				logger.LogCritical("Database health check failed.");
+
+				return StatusCode(503, "Database Unhealthy");
+			}
+		}
+	}
+}
